d3ac6a9844e18c7ad500543bba140cd58e752072
Richard Patel <richxpatel@gmail.com>
2019-02-25 22:00:53 +0100

    Rewrite

--- README.md
+++ README.md
@@ -1,46 +1,9 @@
-# nanoproxy
+# mkhttps
 
-This is a tiny HTTP forward proxy written in [Go],
-for me to gain experience in the Go language.
+__HTTP → HTTPS__
 
-This proxy accepts all requests and forwards them directly to the
-origin server. It performs no caching.
+👁 Quickly turn any HTTP server into HTTPS (with random cert).
 
-Despite this not being a full proxy implementation, it is blazing fast.
-In particular it is significantly faster than Squid and slightly faster than
-Apache's mod_proxy. This demonstrates that Go's built-in HTTP library is
-of a very high quality and that the Go runtime is quite performant.
-
-Only `xkcd.com` has been really tested with this proxy.
-Many other sites don't work with the current implementation.
-
-## Prerequisites
-
-* Go 1.3.3, or a compatible version
-
-## Installation
-
-* Clone this repository.
-
-```
-git clone git@github.com:davidfstr/nanoproxy.git
-cd nanoproxy
-```
-
-* Configure your web browser to route all HTTP traffic through `localhost:8080`.
-
-## Usage
-
-* Start the proxy: `go run nanoproxy.go`
-
-* Open your web browser to `http://xkcd.com` or some other page on that site.
-
-[Go]: https://golang.org
-
-## Notes
-
-* Go's HTTP server implementation is really good. I read it all.
-  Only missing feature I desire is the ability to process multiple
-  pipelined HTTP requests in parallel.
-* Go's HTTP client implementation is easy to use, based on my limited
-  experience in this proxy. I have not read its implementation.
+Usage: `mkhttps localhost:8080 :8081`
+ - Source: `http://localhost:8080/`
+ - New: `https://localhost:8081/`
--- nanoproxy.go
+++ nanoproxy.go
@@ -1,52 +1,59 @@
 package main
 
 import (
+    "crypto/ecdsa"
+    "crypto/elliptic"
+    "crypto/rand"
+    "crypto/x509"
+    "crypto/x509/pkix"
+    "encoding/pem"
     "fmt"
     "io"
+    "log"
+    "math/big"
     "net/http"
+    "os"
+    "os/user"
+    "path/filepath"
     "time"
 )
 
-var verbose = false;
-
-var passthruRequestHeaderKeys = [...]string{
-    "Accept",
-    "Accept-Encoding",
-    "Accept-Language",
-    "Cache-Control",
-    "Cookie",
-    "Referer",
-    "User-Agent",
+var blockedRequestKeys = map[string]bool {
+    "Forwarded": true,
+    "X-Forwarded-For": true,
+    "X-Forwarded-Host": true,
+    "X-Forwarded-Proto": true,
 }
 
-var passthruResponseHeaderKeys = [...]string{
-    "Content-Encoding",
-    "Content-Language",
-    "Content-Type",
-    "Cache-Control", // TODO: Is this valid in a response?
-    "Date",
-    "Etag",
-    "Expires",
-    "Last-Modified",
-    "Location",
-    "Server",
-    "Vary",
-}
+var certFile = ".config/mkhttps.cert"
+var keyFile = ".config/mkhttps.pem"
 
 func main() {
+    usr, err := user.Current()
+    if err != nil { panic(err) }
+    certFile = filepath.Join(usr.HomeDir, certFile)
+    keyFile  = filepath.Join(usr.HomeDir, keyFile)
+
+    if len(os.Args) != 3 {
+        _, _ = fmt.Fprintln(os.Stderr, "Usage: mkhttps <src> <dst>")
+        os.Exit(1)
+    }
+
+    if _, err := os.Stat(keyFile); err != nil {
+        mkcert()
+    }
+
     handler := http.DefaultServeMux
     
     handler.HandleFunc("/", handleFunc)
     
     s := &http.Server{
-        Addr:           ":8080",
-        Handler:        handler,
-        ReadTimeout:    10 * time.Second,
-        WriteTimeout:   10 * time.Second,
-        MaxHeaderBytes: 1 << 20,
+        Addr:    os.Args[2],
+        Handler: handler,
     }
-    
-    s.ListenAndServe()
+
+    log.Print("Listening")
+    log.Fatal(s.ListenAndServeTLS(certFile, keyFile))
 }
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
@@ -54,58 +61,85 @@ func handleFunc(w http.ResponseWriter, r *http.Request) {
     
     // Construct filtered header to send to origin server
     hh := http.Header{}
-    for _, hk := range passthruRequestHeaderKeys {
-        if hv, ok := r.Header[hk]; ok {
-            hh[hk] = hv
+    for k, v := range r.Header {
+        if blockedRequestKeys[k] {
+            continue
         }
+        hh[k] = v
     }
+
+    r.URL.Host = os.Args[1]
     
     // Construct request to send to origin server
-    rr := http.Request{
+    rr := http.Request {
         Method: r.Method,
         URL: r.URL,
         Header: hh,
         Body: r.Body,
-        // TODO: Is this correct for a 0 value?
-        //       Perhaps a 0 may need to be reinterpreted as -1?
-        ContentLength: r.ContentLength,
-        Close: r.Close,
     }
     
     // Forward request to origin server
     resp, err := http.DefaultTransport.RoundTrip(&rr)
     if err != nil {
-        // TODO: Passthru more error information
         http.Error(w, "Could not reach origin server", 500)
         return
     }
     defer resp.Body.Close()
+
+    log.Printf("<-- %v\n", resp.Status)
     
-    if (verbose) {
-        fmt.Printf("<-- %v %+v\n", resp.Status, resp.Header)
-    } else {
-        fmt.Printf("<-- %v\n", resp.Status)
-    }
-    
-    // Transfer filtered header from origin server -> client
-    respH := w.Header()
-    for _, hk := range passthruResponseHeaderKeys {
-        if hv, ok := resp.Header[hk]; ok {
-            respH[hk] = hv
-        }
-    }
+    resp.Header = w.Header()
     w.WriteHeader(resp.StatusCode)
-    
-    // Transfer response from origin server -> client
-    if resp.ContentLength > 0 {
-        // (Ignore I/O errors, since there's nothing we can do)
-        io.CopyN(w, resp.Body, resp.ContentLength)
-    } else if (resp.Close) { // TODO: Is this condition right?
-        // Copy until EOF or some other error occurs
-        for {
-            if _, err := io.Copy(w, resp.Body); err != nil {
-                break
-            }
-        }
+    _, err = io.Copy(w, resp.Body)
+    if err != nil {
+        log.Print(err)
     }
+}
+
+//noinspection ALL
+func mkcert() {
+    priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+    if err != nil { panic(err) }
+
+    notBefore := time.Now()
+    notAfter := notBefore.Add(3 * 365 * 24 * time.Hour)
+
+    serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+    serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+    if err != nil { panic(err) }
+
+    template := x509.Certificate{
+        SerialNumber: serialNumber,
+        Subject: pkix.Name{
+            Organization: []string{"mkhttps"},
+        },
+        NotBefore: notBefore,
+        NotAfter:  notAfter,
+
+        KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+        ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+        BasicConstraintsValid: true,
+    }
+
+    derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+    if err != nil { panic(err) }
+
+    certOut, err := os.Create(certFile)
+    if err != nil { panic(err) }
+    defer certOut.Close()
+
+    if err := pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes}); err != nil {
+        panic(err)
+    }
+
+    privBytes, err := x509.MarshalECPrivateKey(priv)
+    if err != nil { panic(err) }
+    privBlock :=  &pem.Block{Type: "EC PRIVATE KEY", Bytes: privBytes}
+
+    keyOut, err := os.OpenFile(keyFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+    defer keyOut.Close()
+    if err != nil { panic(err) }
+    if err := pem.Encode(keyOut, privBlock); err != nil { panic(err) }
+
+    log.Print("Created new keypair: ", keyFile)
 }
